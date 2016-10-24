db6a3f449a1e2eaaea71d91ae28d05dceeec0e37
SamZapata <legionario.zapata@gmail.com>
2016-10-23 21:05:38 -0500

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021120744) do
+ActiveRecord::Schema.define(version: 20161024014447) do
+
+  create_table "communities", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "parent_community"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
